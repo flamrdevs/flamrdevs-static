89655b2cb8679db4146cf92a4cc02b2b402188a6
flamrdevs <flamrdevs@gmail.com>
2023-06-18 12:03:03 +0700

    chore: styling

---
+++ src/components/core/index.ts
@@ -0,0 +1,5 @@
+export { default as Center } from "./Center.astro";
+export { default as Container } from "./Container.astro";
+export { default as HTMLX } from "./HTMLX.astro";
+export { default as Separator } from "./Separator.astro";
+export { default as Text } from "./Text.astro";
