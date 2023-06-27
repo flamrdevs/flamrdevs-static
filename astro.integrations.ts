bd7493afdf6c7910a482110bd858810109c6cd94
flamrdevs <flamrdevs@gmail.com>
2023-06-28 06:35:08 +0700

    refactor: integrations

---
+++ astro.integrations.ts
@@ -0,0 +1,92 @@
+import fs from "fs/promises";
+import path from "path";
+
+import type { AstroIntegration } from "astro";
+
+const stats = () => {
+	type Directory = {
+		type: "directory";
+		name: string;
+		children: Child[];
+	};
+
+	type File = {
+		type: "file";
+		name: string;
+		ext: string;
+		size: string;
+	};
+
+	type Child = Directory | File;
+
+	const dist = (...paths: string[]) => path.resolve("dist", ...paths);
+
+	const exts = "html,css,js,json,png,svg,ico,ttf".split(",").map((s) => `.${s}`);
+
+	const recursive = async (pathlike: string) => {
+		const children: Child[] = [];
+
+		const dirents = await fs.readdir(pathlike, { withFileTypes: true });
+
+		for (const dirent of dirents) {
+			if (dirent.isDirectory()) {
+				children.push({
+					type: "directory",
+					name: dirent.name,
+					children: await recursive(path.resolve(pathlike, dirent.name)),
+				});
+			} else if (dirent.isFile()) {
+				const ext = path.extname(dirent.name);
+
+				if (exts.includes(ext)) {
+					const { size } = await fs.stat(path.resolve(pathlike, dirent.name));
+					children.push({
+						type: "file",
+						name: dirent.name,
+						ext: ext,
+						size: size >= 1048576 ? `${(size / 1048576).toFixed(2)} MB` : `${(size / 1024).toFixed(2)} KB`,
+					});
+				}
+			}
+		}
+
+		return children;
+	};
+
+	const localeCompareByNameSortFn = <T extends { name: string }>(a: T, b: T) => a.name.localeCompare(b.name, undefined, { numeric: true });
+
+	const sort = (children: Child[]): Child[] => {
+		const directories: Directory[] = [];
+		const files: File[] = [];
+
+		children.forEach((child) => {
+			if (child.type === "directory") {
+				const { type, name, children } = child;
+				directories.push({ type, name, children: sort(children) });
+			} else if (child.type === "file") {
+				files.push(child);
+			}
+		});
+
+		return [...directories.sort(localeCompareByNameSortFn), ...files.sort(localeCompareByNameSortFn)];
+	};
+
+	const start = async () => {
+		await fs.writeFile(dist("stats.json"), JSON.stringify(sort(await recursive(dist()))));
+	};
+
+	return {
+		name: "stats",
+		hooks: {
+			"astro:build:done": async () => {
+				try {
+					await start();
+				} catch (error) {
+					console.error(error);
+				}
+			},
+		},
+	} satisfies AstroIntegration;
+};
+
+export { stats };
