8e1648e993064c4578cc51af5c30c3967b55d545
flamrdevs <flamrdevs@gmail.com>
2023-08-03 13:59:05 +0700

    chore: use props splitter library

--- src/components/core/core.ts
+++ src/components/core/core.ts
@@ -1,37 +1,6 @@
 import { cxs } from "@klass/core";
 import type { ClassValue } from "@klass/core";
 
-type SplitProps<T, K extends (readonly (keyof T)[])[]> = [
-	...{
-		[P in keyof K]: P extends `${number}` ? Pick<T, Extract<K[P], readonly (keyof T)[]>[number]> : never;
-	},
-	Omit<T, K[number][number]>
-];
-
-const splitProps = <T extends Record<any, any>, K extends [readonly (keyof T)[], ...(readonly (keyof T)[])[]]>(props: T, ...keys: K): SplitProps<T, K> => {
-	const otherObject: Record<string, any> = {};
-	const objects: Record<string, any>[] = keys.map(() => ({}));
-
-	for (const prop in props) {
-		let value = props[prop];
-		let blocked = false;
-		let objectIndex = 0;
-		for (const key of keys) {
-			if (key.includes(prop)) {
-				blocked = true;
-				objects[objectIndex][prop] = value;
-			}
-			++objectIndex;
-		}
-
-		if (!blocked) {
-			otherObject[prop] = value;
-		}
-	}
-
-	return [...objects, otherObject] as any;
-};
-
 type ClassesProps = {
 	class?: string | null;
 	"class:list"?: string | Record<string, boolean> | Record<any, any> | Iterable<string> | Iterable<any>;
@@ -89,6 +58,5 @@ const getSeparatorAttrs = ({ orientation = "horizontal" }: SeparatorOptions) => 
 };
 
 export type { ClassesProps, ButtonOptions, LinkOptions, SeparatorOptions };
-export { splitProps };
 export { getRootClassesx };
 export { getButtonAttrs, getLinkAttrs, getSeparatorAttrs };
