d78364fd7f5b53a4a54ff9a628149ebb0d0a17d5
flamrdevs <flamrdevs@gmail.com>
2023-10-06 04:45:33 +0700

    build: update stats format

--- astro.integrations.ts
+++ astro.integrations.ts
@@ -4,67 +4,82 @@ import path from "path";
 import type { AstroIntegration } from "astro";
 
 const stats = (options: { exts: string[] }) => {
-	type Directory = {
+	type Directory<T extends string | number = number> = {
 		type: "directory";
 		name: string;
-		children: Child[];
+		children: Child<T>[];
+		size: T;
 	};
 
-	type File = {
+	type File<T extends string | number = number> = {
 		type: "file";
 		name: string;
 		ext: string;
-		size: string;
+		size: T;
 	};
 
-	type Child = Directory | File;
+	type Child<T extends string | number = number> = Directory<T> | File<T>;
 
 	const dist = (...paths: string[]) => path.resolve("dist", ...paths);
 
 	const exts = options.exts.map((s) => `.${s}`);
 
 	const recursive = async (pathlike: string) => {
-		const children: Child[] = [];
+		const result: {
+			children: Child<number>[];
+			size: number;
+		} = {
+			children: [],
+			size: 0,
+		};
 
 		const dirents = await fs.readdir(pathlike, { withFileTypes: true });
 
 		for (const dirent of dirents) {
 			if (dirent.isDirectory()) {
-				children.push({
+				const { children, size } = await recursive(path.resolve(pathlike, dirent.name));
+
+				result.children.push({
 					type: "directory",
 					name: dirent.name,
-					children: await recursive(path.resolve(pathlike, dirent.name)),
+					children,
+					size: size,
 				});
+				result.size += size;
 			} else if (dirent.isFile()) {
 				const ext = path.extname(dirent.name);
 
 				if (exts.includes(ext)) {
 					const { size } = await fs.stat(path.resolve(pathlike, dirent.name));
-					children.push({
+					result.children.push({
 						type: "file",
 						name: dirent.name,
 						ext: ext,
-						size: size >= 1048576 ? `${(size / 1048576).toFixed(2)} MB` : `${(size / 1024).toFixed(2)} KB`,
+						size: size,
 					});
+					result.size += size;
 				}
 			}
 		}
 
-		return children;
+		return result;
 	};
+
+	const format = (size: number) => (size >= 1048576 ? `${(size / 1048576).toFixed(2)} MB` : `${(size / 1024).toFixed(2)} KB`);
 
 	const localeCompareByNameSortFn = <T extends { name: string }>(a: T, b: T) => a.name.localeCompare(b.name, undefined, { numeric: true });
 
-	const sort = (children: Child[]): Child[] => {
-		const directories: Directory[] = [];
-		const files: File[] = [];
+	const friendly = (children: Child<number>[]): Child<string>[] => {
+		const directories: Directory<string>[] = [];
+		const files: File<string>[] = [];
 
 		children.forEach((child) => {
 			if (child.type === "directory") {
-				const { type, name, children } = child;
-				directories.push({ type, name, children: sort(children) });
+				const { type, name, children, size } = child;
+				directories.push({ type, name, children: friendly(children), size: format(size) });
 			} else if (child.type === "file") {
-				files.push(child);
+				const { type, name, ext, size } = child;
+				files.push({ type, name, ext, size: format(size) });
 			}
 		});
 
@@ -72,7 +87,14 @@ const stats = (options: { exts: string[] }) => {
 	};
 
 	const start = async () => {
-		await fs.writeFile(dist("stats.json"), JSON.stringify(sort(await recursive(dist()))));
+		const { children, size } = await recursive(dist());
+		const root: Directory<string> = {
+			type: "directory",
+			name: "",
+			children: friendly(children),
+			size: format(size),
+		};
+		await fs.writeFile(dist("stats.json"), JSON.stringify(root));
 	};
 
 	return {
